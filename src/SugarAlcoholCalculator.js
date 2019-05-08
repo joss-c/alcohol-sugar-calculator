01dc2595e2c2dc90f049fd143600234a22c932d0
joss-c <joss.classey@gmail.com>
2019-05-08 20:24:33 +0100

    feat(conversion): Option to convert in Oz

--- src/SugarAlcoholCalculator.js
+++ src/SugarAlcoholCalculator.js
@@ -3,12 +3,20 @@ import './App.css';
 
 function SugarAlcoholCalculator() {
   const [ unit, setUnit ] = useState(0)
+  const [ selectedUnit, selectUnit ] = useState('unit')
   useEffect(() => {
     console.log(unit)
   })
   const gramsPerUnit = (unit * 61 / 4).toFixed(2)
+  const gramsPerOunce =  parseFloat((gramsPerUnit / 100) * 20) + parseFloat(gramsPerUnit)
+  const unitTypes = {
+    'unit': (unit * 61 / 4).toFixed(2),
+    'oz': parseFloat((gramsPerUnit / 100) * 20) + parseFloat(gramsPerUnit)
+  }
   const caloriesPerUnit = (unit * 61).toFixed(2)
   const minsOfFrisbee = (caloriesPerUnit / 61 * 20.06).toFixed(2)
+  const mlPerUnit = (unit * 25).toFixed(2)
+  const mlToOz = (mlPerUnit * 0.03519503).toFixed(4)
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +24,20 @@ function SugarAlcoholCalculator() {
       </header>
       <body>
         <p>Alcohol intake converted to relative sugar quantity</p>
-        {'Units: '}
+        <span
+          style={{ cursor: 'pointer' }}
+          onClick={
+            () => selectUnit(
+              (selectedUnit === 'unit')
+                ? 'oz'
+                : 'unit')
+          }
+        >
+          {(selectedUnit === 'unit')
+            ? 'Units (25ml): '
+            : 'Ounces: '
+          }
+        </span>
         <input
           name='units'
           type='number'
@@ -24,16 +45,18 @@ function SugarAlcoholCalculator() {
           value={unit}
           defaultValue='0'
         />
-        <p>{(gramsPerUnit > 1000) ? (gramsPerUnit / 1000).toFixed(2) + 'kg of sugar' : gramsPerUnit + ' grams of sugar'}</p>
-        <p>{Math.floor((gramsPerUnit) / 3)} cubes of sugar</p>
-        <p>{Math.floor((gramsPerUnit) / 51.67 * 100)}% RDA for sugar</p>
-        <p>{(gramsPerUnit / 26.05).toFixed(2)} mars bars (sugar content)</p>
+        <p>{(unitTypes[selectedUnit] > 1000) ? (unitTypes[selectedUnit] / 1000).toFixed(2) + 'kg of sugar' : unitTypes[selectedUnit] + ' grams of sugar'}</p>
+        <p>{Math.floor((unitTypes[selectedUnit]) / 3)} cubes of sugar</p>
+        <p>{Math.floor((unitTypes[selectedUnit]) / 51.67 * 100)}% RDA for sugar</p>
+        <p>{(unitTypes[selectedUnit] / 26.05).toFixed(1)} mars bars (sugar content)</p>
         {(minsOfFrisbee > 60)
-          ? (minsOfFrisbee / 60).toFixed(2) + ' hours of frisbee'
+          ? (minsOfFrisbee / 60).toFixed(1) + ' hours of frisbee'
           : minsOfFrisbee + ' minutes of frisbee'
         }
         <p>Calories: {Math.floor(caloriesPerUnit)}</p>
         <p>{(Math.floor(caloriesPerUnit) / 2000 * 100).toFixed(2)}% RDA of calories (2000/kcal)</p>
+        <p>ml: {mlPerUnit}</p>
+        <p>ml to oz: {(Number.isInteger(mlPerUnit * 0.03519503)) ? mlToOz : parseFloat(mlToOz).toFixed(2)}</p>
       </body>
       <footer>
         <p></p>
