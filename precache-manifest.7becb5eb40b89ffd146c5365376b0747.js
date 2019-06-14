56c2357a98a9b13de49199866290559fc5651c37
joss-c <joss.classey@gmail.com>
2019-06-14 15:48:00 +0100

    Updates

---
+++ precache-manifest.7becb5eb40b89ffd146c5365376b0747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92e4524172c3e197a53e3fd054d1c63",
+    "url": "/alcohol-sugar-calculator/index.html"
+  },
+  {
+    "revision": "95cf77a9f7e54b75b330",
+    "url": "/alcohol-sugar-calculator/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "abb5d27ba5e1aac5d48c",
+    "url": "/alcohol-sugar-calculator/static/css/main.8eb92fd0.chunk.css"
+  },
+  {
+    "revision": "95cf77a9f7e54b75b330",
+    "url": "/alcohol-sugar-calculator/static/js/2.a0acb93a.chunk.js"
+  },
+  {
+    "revision": "abb5d27ba5e1aac5d48c",
+    "url": "/alcohol-sugar-calculator/static/js/main.4b0ac796.chunk.js"
+  },
+  {
+    "revision": "147f126a14ded5f194f9",
+    "url": "/alcohol-sugar-calculator/static/js/runtime~main.56414c12.js"
+  }
+]);
